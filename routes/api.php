b9715d037eff6594b490908773ce3e34cee33f81
Lukyrouge <florian@deneria.net>
2026-07-22 08:48:18 +0200

    Clean

--- routes/api.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
-
-Route::middleware('auth:sanctum')->group(function () {
-    Route::apiResource('dns-entries', \App\Http\Controllers\DNSEntryController::class);
-});
